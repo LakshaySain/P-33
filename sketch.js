143a1781d5301b622d29f6c80515dc9818383baa
LakshaySain <73537063+LakshaySain@users.noreply.github.com>
2021-01-09 17:33:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,285 @@
+var b1,b2,b3,b4,b5,b6,b7,b8,b9,b0,b11,b12,b13,b14,b15,b16,b17,b18,b19,b20,b21;
+
+var w1,w2,w3,w4,w5,w6,w7,w8,w9,w10;
+
+
+function setup() {
+  createCanvas(800,500);
+  b1 =  createSprite(200, 200, 10, 10);
+  b1.shapeColor="white";
+
+  b2 =  createSprite(150, 200, 10, 10);
+  b2.shapeColor="white";
+
+  b3 =  createSprite(100, 200, 10, 10);
+  b3.shapeColor="white";
+
+  b4 =  createSprite(50, 200, 10, 10);
+  b4.shapeColor="white";
+
+  b5 =  createSprite(250, 200, 10, 10);
+  b5.shapeColor="white";
+
+  b6 =  createSprite(300, 200, 10, 10);
+  b6.shapeColor="white";
+
+  b6 =  createSprite(350, 200, 10, 10);
+  b6.shapeColor="white";
+
+  b7 =  createSprite(400, 200, 10, 10);
+  b7.shapeColor="white";
+
+  b8 =  createSprite(450, 200, 10, 10);
+  b8.shapeColor="white";
+
+  b9 =  createSprite(500, 200, 10, 10);
+  b9.shapeColor="white";
+
+  b0 =  createSprite(550, 200, 10, 10);
+  b0.shapeColor="white";
+
+  b11 =  createSprite(600, 200, 10, 10);
+  b11.shapeColor="white";
+
+  b12 =  createSprite(650, 200, 10, 10);
+  b12.shapeColor="white";
+
+  b13 =  createSprite(700, 200, 10, 10);
+  b13.shapeColor="white";
+
+  b14 =  createSprite(750, 200, 10, 10);
+  b14.shapeColor="white";
+   
+
+
+
+
+
+  b15 =  createSprite(225, 150, 10, 10);
+  b15.shapeColor="white";
+
+  b16 =  createSprite(275, 150, 10, 10);
+  b16.shapeColor="white";
+
+  b17 =  createSprite(325, 150, 10, 10);
+  b17.shapeColor="white";
+
+  b18 =  createSprite(375, 150, 10, 10);
+  b18.shapeColor="white";
+
+  b19 =  createSprite(425,150, 10, 10);
+  b19.shapeColor="white";
+
+  b20 =  createSprite(475, 150, 10, 10);
+  b20.shapeColor="white";
+
+  b21=  createSprite(525, 150, 10, 10);
+  b21.shapeColor="white";
+
+  
+  b22=  createSprite(575, 150, 10, 10);
+  b22.shapeColor="white";
+
+  b23=  createSprite(625, 150, 10, 10);
+  b23.shapeColor="white";
+
+  b24=  createSprite(675, 150, 10, 10);
+  b24.shapeColor="white";
+
+  b25=  createSprite(725, 150, 10, 10);
+  b25.shapeColor="white";
+
+  b26=  createSprite(775, 150, 10, 10);
+  b26.shapeColor="white";
+
+  b27=  createSprite(25, 150, 10, 10);
+  b27.shapeColor="white";
+
+  b28=  createSprite(75, 150, 10, 10);
+  b28.shapeColor="white";
+
+  b29=  createSprite(125, 150, 10, 10);
+  b29.shapeColor="white";
+
+  b30=  createSprite(175, 150, 10, 10);
+  b30.shapeColor="white";
+
+  
+
+
+
+
+  b1 =  createSprite(200, 100, 10, 10);
+  b1.shapeColor="white";
+
+  b2 =  createSprite(150, 100, 10, 10);
+  b2.shapeColor="white";
+
+  b3 =  createSprite(100, 100, 10, 10);
+  b3.shapeColor="white";
+
+  b4 =  createSprite(50, 100, 10, 10);
+  b4.shapeColor="white";
+
+  b5 =  createSprite(250, 100, 10, 10);
+  b5.shapeColor="white";
+
+  b6 =  createSprite(300, 100, 10, 10);
+  b6.shapeColor="white";
+
+  b6 =  createSprite(350, 100, 10, 10);
+  b6.shapeColor="white";
+
+  b7 =  createSprite(400, 100, 10, 10);
+  b7.shapeColor="white";
+
+  b8 =  createSprite(450, 100, 10, 10);
+  b8.shapeColor="white";
+
+  b9 =  createSprite(500, 100, 10, 10);
+  b9.shapeColor="white";
+
+  b0 =  createSprite(550, 100, 10, 10);
+  b0.shapeColor="white";
+
+  b11 =  createSprite(600, 100, 10, 10);
+  b11.shapeColor="white";
+
+  b12 =  createSprite(650, 100, 10, 10);
+  b12.shapeColor="white";
+
+  b13 =  createSprite(700, 100, 10, 10);
+  b13.shapeColor="white";
+
+  b14 =  createSprite(750, 100, 10, 10);
+  b14.shapeColor="white";
+  
+
+
+
+
+
+
+  b15 =  createSprite(225, 50, 10, 10);
+  b15.shapeColor="white";
+
+  b16 =  createSprite(275, 50, 10, 10);
+  b16.shapeColor="white";
+
+  b17 =  createSprite(325, 50, 10, 10);
+  b17.shapeColor="white";
+
+  b18 =  createSprite(375, 50, 10, 10);
+  b18.shapeColor="white";
+
+  b19 =  createSprite(425,50, 10, 10);
+  b19.shapeColor="white";
+
+  b20 =  createSprite(475, 50, 10, 10);
+  b20.shapeColor="white";
+
+  b21=  createSprite(525, 50, 10, 10);
+  b21.shapeColor="white";
+
+  
+  b22=  createSprite(575, 50, 10, 10);
+  b22.shapeColor="white";
+
+  b23=  createSprite(625, 50, 10, 10);
+  b23.shapeColor="white";
+
+  b24=  createSprite(675, 50, 10, 10);
+  b24.shapeColor="white";
+
+  b25=  createSprite(725, 50, 10, 10);
+  b25.shapeColor="white";
+
+  b26=  createSprite(775, 50, 10, 10);
+  b26.shapeColor="white";
+
+  b27=  createSprite(25, 50, 10, 10);
+  b27.shapeColor="white";
+
+  b28=  createSprite(75, 50, 10, 10);
+  b28.shapeColor="white";
+
+  b29=  createSprite(125, 50, 10, 10);
+  b29.shapeColor="white";
+
+  b30=  createSprite(175, 50, 10, 10);
+  b30.shapeColor="white";
+
+  
+
+
+
+
+
+
+
+
+
+  w1 =  createSprite(200, 400, 7, 300);
+  w1.shapeColor="white";
+
+  w1 =  createSprite(0, 400, 7, 300);
+  w1.shapeColor="white";
+
+  w2=  createSprite(100, 400, 7, 300);
+  w2.shapeColor="white";
+
+  w3 =  createSprite(200, 400, 7, 300);
+  w3.shapeColor="white";
+
+  w4 =  createSprite(300, 400, 7, 300);
+  w4.shapeColor="white";
+
+  w5 =  createSprite(400, 400, 7, 300);
+  w5.shapeColor="white";
+
+  w6 =  createSprite(500, 400, 7, 300);
+  w6.shapeColor="white";
+
+  w7 =  createSprite(600, 400, 7, 300);
+  w7.shapeColor="white";
+
+  w8 =  createSprite(700, 400, 7, 300);
+  w8.shapeColor="white";
+  
+  
+  w10 =  createSprite(790, 400, 7, 300);
+  w10.shapeColor="white";
+
+
+
+  w9 =  createSprite(200,485,1300, 10);
+  w9.shapeColor="white";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+function draw() {
+  background("black");  
+  drawSprites();
+}
